2d68e3aabd90459e0a87e7bf0c28e8da7851f62f
westoshy <t.nishimura020830@gmail.com>
2017-05-28 12:16:35 +0900

    first commit

---
+++ test1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(void)
+{
+  std::cout << "test1" << std::endl;
+  return 0;
+}
---
+++ test2.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(void)
+{
+  std::cout << "test2" << std::endl;
+  return 0;
+}
